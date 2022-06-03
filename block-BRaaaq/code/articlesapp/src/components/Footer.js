5852efea2871e23ec54699a4d3cb5889d6f8bea0
priya pandey <priyarock746@gmail.com>
2022-06-03 16:58:52 +0530

    Assignment

---
+++ block-BRaaaq/code/articlesapp/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer (){
+    return(
+        <footer>
+            <div className="container flex footer">
+                <small>&copy; 2022  All rights reserved..</small>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
